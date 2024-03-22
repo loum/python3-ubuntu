e0c7bcf0747ed4e9511156c23561e2f29a6f8741
Lou Markovski <lou.markovski@gmail.com>
2024-03-23 02:16:03 +1100

    35-lifecycle-management-jammy-20230227

--- scripts/bulkbuild.sh
+++ scripts/bulkbuild.sh
@@ -4,43 +4,43 @@
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.8.17\
- -t loum/python3-ubuntu:jammy-3.8.17 .
+ --build-arg PYTHON3_VERSION=3.8.18\
+ -t loum/python3-ubuntu:jammy-3.8.18 .
 
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.8.18\
+ --build-arg PYTHON3_VERSION=3.8.19\
  -t loum/python3-ubuntu:jammy-3.8\
- -t loum/python3-ubuntu:jammy-3.8.18 .
+ -t loum/python3-ubuntu:jammy-3.8.19 .
 
 # Python 3.9.
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.9.17\
- -t loum/python3-ubuntu:jammy-3.9.17 .
+ --build-arg PYTHON3_VERSION=3.9.18\
+ -t loum/python3-ubuntu:jammy-3.9.18 .
 
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.9.18\
+ --build-arg PYTHON3_VERSION=3.9.19\
  -t loum/python3-ubuntu:jammy-3.9\
- -t loum/python3-ubuntu:jammy-3.9.18 .
+ -t loum/python3-ubuntu:jammy-3.9.19 .
 
 # Python 3.10.
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.10.12\
- -t loum/python3-ubuntu:jammy-3.10.12 .
+ --build-arg PYTHON3_VERSION=3.10.13\
+ -t loum/python3-ubuntu:jammy-3.10.13 .
 
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.10.13\
+ --build-arg PYTHON3_VERSION=3.10.14\
  -t loum/python3-ubuntu:jammy-3.10\
- -t loum/python3-ubuntu:jammy-3.10.13 .
+ -t loum/python3-ubuntu:jammy-3.10.14 .
 
 # Python 3.11.
 docker buildx build --platform linux/arm64,linux/amd64\
