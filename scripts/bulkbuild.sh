0e83e7765077e4ed191b1e07a820a5ae5a73e34d
Lou Markovski <lou.markovski@gmail.com>
2024-04-25 20:11:07 +1000

    37-lifecycle-management-python-3123

--- scripts/bulkbuild.sh
+++ scripts/bulkbuild.sh
@@ -7,7 +7,7 @@ docker buildx build --platform linux/arm64,linux/amd64\
  --build-arg PYTHON3_VERSION=3.8.18\
  -t loum/python3-ubuntu:jammy-3.8.18 .
 
-docker buildx build --platform linux/arm64,linux/amd64\
+#docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
  --build-arg PYTHON3_VERSION=3.8.19\
@@ -21,7 +21,7 @@ docker buildx build --platform linux/arm64,linux/amd64\
  --build-arg PYTHON3_VERSION=3.9.18\
  -t loum/python3-ubuntu:jammy-3.9.18 .
 
-docker buildx build --platform linux/arm64,linux/amd64\
+#docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
  --build-arg PYTHON3_VERSION=3.9.19\
@@ -46,28 +46,27 @@ docker buildx build --platform linux/arm64,linux/amd64\
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.11.7\
- -t loum/python3-ubuntu:jammy-3.11.7 .
+ --build-arg PYTHON3_VERSION=3.11.8\
+ -t loum/python3-ubuntu:jammy-3.11.8 .
 
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.11.8\
+ --build-arg PYTHON3_VERSION=3.11.9\
  -t loum/python3-ubuntu:jammy-3.11\
- -t loum/python3-ubuntu:jammy-3.11.8\
- -t loum/python3-ubuntu:latest .
+ -t loum/python3-ubuntu:jammy-3.11.9\
 
 # Python 3.12 and latest.
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.12.1\
- -t loum/python3-ubuntu:jammy-3.12.1 .
+ --build-arg PYTHON3_VERSION=3.12.2\
+ -t loum/python3-ubuntu:jammy-3.12.2 .
 
 docker buildx build --platform linux/arm64,linux/amd64\
  --push --rm --no-cache\
  --build-arg UBUNTU_BASE_IMAGE=$UBUNTU_BASE_IMAGE\
- --build-arg PYTHON3_VERSION=3.12.2\
+ --build-arg PYTHON3_VERSION=3.12.3\
  -t loum/python3-ubuntu:jammy-3.12\
- -t loum/python3-ubuntu:jammy-3.12.2\
+ -t loum/python3-ubuntu:jammy-3.12.3\
  -t loum/python3-ubuntu:latest .
